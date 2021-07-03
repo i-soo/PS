6a7b5f7f6cfc9eae0f6bdde36d2ebd848582e8fe
i-soo <58316484+i-soo@users.noreply.github.com>
2021-07-03 23:31:43 +0900

    Create 2562.py

---
+++ BOJ/Steps/Step5/2562.py
@@ -0,0 +1,10 @@
+max_num = 0
+max_index = 0
+
+for i in range(9):
+    a = int(input())
+    if(a > max_num):
+        max_num = a
+        max_index = i + 1
+        
+print('%d\n%d' %(max_num, max_index))
