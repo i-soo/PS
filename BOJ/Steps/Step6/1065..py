200c61b69b6d7f06c21868056d5e4130e8d50f6f
i-soo <58316484+i-soo@users.noreply.github.com>
2021-07-05 23:59:58 +0900

    Create 1065..py

---
+++ BOJ/Steps/Step6/1065..py
@@ -0,0 +1,18 @@
+# 한수 개수를 카운트하는 함수 정의
+# 한수: 양의 정수이면서 각 자리가 등차수열을 이루는 경우
+def count_hansu(n):
+        cnt = 0
+        if (n < 100):
+                return n
+        else:
+	         cnt = 99
+           for i in range(100, N+1)
+	            num_list = list(map(int, str(i)))
+
+              if  num_list[0] - num_list[1] == num_list[1] - num_list[2] :
+                   cnt += 1
+           return cnt
+ 
+inp = int(input())
+result = count_hansu(inp)
+print(result)
